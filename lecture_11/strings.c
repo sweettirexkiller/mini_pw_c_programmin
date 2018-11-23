e5e6a2dc567c3f31ed831d18da79a12b8d12ccc8
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-11-23 10:32:30 +0100

    string library main functions

---
+++ lecture_11/strings.c
@@ -0,0 +1,28 @@
+// 
+// Created by  Peter Yankievich on 23/11/2018.
+//
+
+#include "stdio.h"
+#include "string.h"
+
+#define N 5;
+int main(int argc, char *argv[]) {
+    //char tab[N]; // string
+    /*
+     * String library functions:
+     *
+     * - size_t strlen(const char *s) //  STRING LENGTH -- site_t is unsigned integer
+     *
+     * - int strcmp(const char * s1, const char * s2) // COMPARE
+     *
+     * - char * strcpy(const char* dest, const char *src) // COPY
+     *
+     * - char * strcat(char * dest, const char * src) // CONCATENATION
+     *
+     * - void * memset(void *s, int c, size_t n) // MEMORY SET
+     *
+     * - void * memcpy(void * dest, const void * source, size_t n) // MEMORY COPY
+     *
+     */
+    return 0;
+}
