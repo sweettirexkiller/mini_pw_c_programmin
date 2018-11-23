eb8a6e55d5469c8e9bcff9b14ec017cc46843306
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-11-23 11:57:48 +0100

    finished copying from blacxkborad

---
+++ lecture_11/notes.c
@@ -0,0 +1,21 @@
+// 
+// Created by  Peter Yankievich on 23/11/2018.
+//
+
+//char tab[N]; // string
+/*
+ * String library functions:
+ *
+ * - size_t strlen(const char *s) //  STRING LENGTH -- site_t is unsigned integer
+ *
+ * - int strcmp(const char * s1, const char * s2) // COMPARE
+ *
+ * - char * strcpy(const char* dest, const char *src) // COPY
+ *
+ * - char * strcat(char * dest, const char * src) // CONCATENATION
+ *
+ * - void * memset(void *s, int c, size_t n) // MEMORY SET
+ *
+ * - void * memcpy(void * dest, const void * source, size_t n) // MEMORY COPY
+ *
+ */
