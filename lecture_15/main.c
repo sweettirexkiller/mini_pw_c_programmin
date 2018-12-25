792bb937840b3161ffb3067cfbae0cc3b7eeaebc
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-12-25 15:02:30 +0100

    update from last lecture

---
+++ lecture_15/main.c
@@ -0,0 +1,91 @@
+// 
+// Created by  Peter Yankievich on 25/12/2018.
+//
+#include<stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int key;
+    struct node *left, *right;
+};
+
+typedef struct node nd;
+
+nd *new_node(int item);
+
+nd *insert(nd *node, int key);
+
+void browse_inorder(nd *root);
+
+void browse_preorder(nd *root);
+
+void browse_postorder(nd *root);
+
+void postorder_destroyer(nd *root);
+
+int main() {
+    nd *root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    browse_preorder(root);
+
+    browse_inorder(root);
+
+    browse_postorder(root);
+
+    postorder_destroyer(root);
+
+    return 0;
+}
+
+nd *new_node(int item) {
+    nd *tmp = (nd *) malloc(sizeof(nd));
+    tmp->key = item;
+    tmp->left = tmp->right = NULL;
+    return tmp;
+}
+
+nd *insert(nd *node, int key) {
+    if (node == NULL) return new_node(key);
+    if (key < node->key) {
+        node->left = insert(node->left, key);
+    }
+    else if(key > node->key){
+        node->left = insert(node->right, key);
+    }
+    return node;
+}
+//20,40,30,60,80,70,50
+void browse_postorder(nd *root){
+    if(root != NULL){
+        browse_postorder(root->left);
+        browse_postorder(root->right);
+        printf("%d\n", root->key);
+    }
+}
+//20.30.40.50.60.70.80
+void browse_inorder(nd *root){
+    if(root != NULL){
+        browse_inorder(root->left);
+        printf("%d\n", root->key);
+        browse_inorder(root->right);
+    }
+}
+//50,40,20,40,70,60,80
+void browse_preorder(nd *root){
+    if(root != NULL){
+        printf("%d\n", root->key);
+        browse_preorder(root->left);
+        browse_preorder(root->right);
+    }
+}
+
+void postorder_destroyer(nd *root){
+
+}
